acbd8374c2514f615cf67ae3afea2d7093b40fad
tzk07 <2499307660@qq.com>
2025-12-11 18:34:46 +0800

    Leetcode454-4sum-ii

--- HashTable/Leetcode454-4sum-ii.cpp
+++ HashTable/Leetcode454-4sum-ii.cpp
@@ -1,3 +1,4 @@
+#include <unordered_map>
 #include <vector>
 
 using namespace std;
@@ -11,6 +12,27 @@ using namespace std;
 class Solution {
 public:
     int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
-        
+        // 题目只需要统计次数而不用输出具体的四元组
+        /*
+            步骤:
+            1、首先定义 一个unordered_map，key放a和b两数之和，value 放a和b两数之和出现的次数。
+            2、遍历大A和大B数组，统计两个数组元素之和，和出现的次数，放到map中。
+            3、定义int变量count，用来统计 a+b+c+d = 0 出现的次数。
+            4、再遍历大C和大D数组，找到如果 0-(c+d) 在map中出现过的话，就用count把map中key对应的value也就是出现次数统计出来。
+            5、最后返回统计值 count 就可以了
+        */
+        unordered_map<int, int> map;
+        for (int a : nums1){
+            for (int b : nums2){
+                map[a + b]++; 
+            }
+        }
+        int count = 0;
+        for (int c : nums3){
+            for (int d : nums4){
+                count += map[0-(c + d)]; 
+            }
+        }
+        return count;
     }
 };
